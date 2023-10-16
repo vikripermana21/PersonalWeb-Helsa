e322a4aee69fd9bbbff7392ba6e8c69b40cef11a
helsaalika <helsa.alika.tif421@polban.ac.id>
2023-10-16 16:05:55 +0700

    new branch for backup

--- frontend/src/components/CV/convertToWeb.js
+++ frontend/src/components/CV/convertToWeb.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import { AiOutlineInstagram } from "react-icons/ai";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 
 const ConvertToWeb = () => {
+
   return (
-    <div>
-    </div>
+    <div></div>
   )
 }
 
